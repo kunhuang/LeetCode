fcb10c4e91ff5c3b0cf95404e4c38d1c20c5d401
kunhuang <kunhuang110@126.com>
2015-10-15 21:40:59 -0700

    47

---
+++ 047_Permutations II.py
@@ -0,0 +1,33 @@
+import pdb
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        hash_ = {}
+        for num in nums:
+            hash_[num] = hash_.get(num, 0) + 1
+
+        result = [[]]
+        tmp_result = []
+
+        while True:
+            for permutation in result:
+                if len(permutation) < len(nums):
+                    for num in hash_.keys():
+                        if len(permutation) + hash_[num] - permutation.count(num) == len(nums):
+                            tmp_result.append(permutation+[num]*(hash_[num] - permutation.count(num)))
+                        elif len(permutation) == 0 or permutation[-1] != num:
+                            for j in range(hash_[num] - permutation.count(num)):
+                                tmp_result.append(permutation+[num]*(j+1))
+                elif len(permutation) == len(nums):
+                    tmp_result.append(permutation)
+            result = tmp_result[:]
+            tmp_result = []
+            if min([len(permutation) for permutation in result]) == len(nums):
+                break
+
+        return result
+
+print Solution().permuteUnique([1,1,3])
