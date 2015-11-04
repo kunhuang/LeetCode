bd71d33afe476234586b6f53cd200d860968145b
kunhuang <kunhuang110@126.com>
2015-11-04 13:26:54 -0800

    274

---
+++ 274_H-Index.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort()
+        if len(citations) == 0:
+            return 0
+        for i in range(1, len(citations)+1):
+            if citations[len(citations)-i] > i:
+                continue
+            elif citations[len(citations)-i] == i:
+                return i
+            else:
+                return i - 1
+
+        return i
+
+citations = [0,0,0,0]
+print Solution().hIndex(citations)
